4b70daddafc1935c9caf8697e1697141bbc2c45f
PageKallop <PageMarie78@gmail.com>
2021-01-03 18:26:48 -0600

    Set up plane detection Imported grid

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -43,15 +43,15 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         
         // Create a new scene
-        let diceScene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
-        
-        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-        
-        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-        
-        sceneView.scene.rootNode.addChildNode(diceNode)
-       
-        }
+//        let diceScene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
+//
+//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+//
+//        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
+//
+//        sceneView.scene.rootNode.addChildNode(diceNode)
+//
+//        }
 //
 //        // Set the scene to the view
 //        sceneView.scene = scene
@@ -77,10 +77,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
+    //detects plain
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
-            print("plane detected")
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat( planeAnchor.extent.z))
+            
+            let planeNode = SCNNode()
+            
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            let gridMaterial = SCNMaterial()
+            
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
         } else {
             return
         }
